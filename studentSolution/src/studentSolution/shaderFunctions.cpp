a6b92fc18ec41b060c95c27654edf1f4a113ffb7
xpeland00 <pelanekd@gmail.com>
2026-05-19 02:08:45 +0200

    Texture sampling: add fast paths for common U8-RGBA and F32-R formats

--- studentSolution/src/studentSolution/shaderFunctions.cpp
+++ studentSolution/src/studentSolution/shaderFunctions.cpp
@@ -11,7 +11,7 @@
  */
 glm::vec4 student_read_texture(Texture const&texture,glm::vec2 const&uv){
   if(!texture.img.data)return glm::vec4(0.f);
-  auto uv1 = glm::fract(glm::fract(uv)+1.f);
+  auto uv1 = glm::fract(uv);
   auto uv2 = uv1*glm::vec2(texture.width-1,texture.height-1)+0.5f;
   auto pix = glm::uvec2(uv2);
   return student_texelFetch(texture,pix);
@@ -46,12 +46,18 @@ glm::vec4 student_texelFetch(Texture const&texture,glm::uvec2 const&pix){
   glm::vec4 color = glm::vec4(0.f,0.f,0.f,1.f);
   if(pix.x>=texture.width || pix.y >=texture.height)return color;
   if(img.format == Image::U8){
-    auto colorPtr = (uint8_t*)getPixel(img,pix.x,pix.y);
+    auto colorPtr = (uint8_t*)img.data + pix.y * img.pitch + pix.x * img.bytesPerPixel;
+    if(img.channels==4 && img.channelTypes[0]==Image::RED && img.channelTypes[1]==Image::GREEN && img.channelTypes[2]==Image::BLUE && img.channelTypes[3]==Image::ALPHA){
+      return glm::vec4(colorPtr[0]/255.f,colorPtr[1]/255.f,colorPtr[2]/255.f,colorPtr[3]/255.f);
+    }
     for(uint32_t c=0;c<img.channels;++c)
       color[c] = colorPtr[img.channelTypes[c]]/255.f;
   }
-  if(texture.img.format == Image::F32){
-    auto colorPtr = (float*)getPixel(img,pix.x,pix.y);
+  else if(img.format == Image::F32){
+    auto colorPtr = (float*)((uint8_t*)img.data + pix.y * img.pitch + pix.x * img.bytesPerPixel);
+    if(img.channels==1 && img.channelTypes[0]==Image::RED){
+      return glm::vec4(*colorPtr,0.f,0.f,1.f);
+    }
     for(uint32_t c=0;c<img.channels;++c)
       color[c] = colorPtr[img.channelTypes[c]];
   }
